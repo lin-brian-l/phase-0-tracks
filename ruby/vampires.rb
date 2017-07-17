b31df8ff7b6e7c58fe9add6eb76f68b567ba8fc3
Brian Lin <lin.brian.l@gmail.com>
2017-07-17 18:01:10 -0500

    Complete Release 3.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,18 +1,40 @@
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.to_i
-puts "What year were you born?"
-year = gets.to_i
-puts "Our company serves garlic bread. Should we order some for you (y/n)"
-order = gets.chomp
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance = gets.chomp
+puts "How many employees will be processed?"
+repeat = gets.chomp.to_i
+for i in 1..repeat
+	puts
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.to_i
+	puts "What year were you born?"
+	year = gets.to_i
+	puts "Our company serves garlic bread. Should we order some for you (y/n)"
+	order = gets.chomp.downcase
+	ordered_garlic = order == "y"
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
+	insurance = gets.chomp.downcase
+	enroll_insurance = insurance == "y"
 
-puts case
-	when name == ("Drake Cula" || "Tu Fang")
-		puts "Definitely a vampire."
-	when (age == 2017 - year) && (order.downcase == "y" || insurance.downcase == "y")
-		puts "Probably not a vampire."
-	
-end 
+	correct_age = (age == 2017 - year)
+
+	result = "Results inconclusive"
+
+	if correct_age && (ordered_garlic || enroll_insurance)
+		result = "Probably not a vampire."
+	end
+
+	if (correct_age == false) && (!ordered_garlic || !enroll_insurance)
+		result = "Probably a vampire."
+	end
+
+	if (correct_age == false) && (!ordered_garlic) && (!enroll_insurance)
+		result = "Almost certainly a vampire."
+	end 
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		result = "Definitely a vampire."
+	end
+
+	puts
+	puts result
+end
