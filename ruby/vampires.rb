45ad7133ee96512062801e9bfd1dc83468a0965f
Brian Lin <lin.brian.l@gmail.com>
2017-07-17 16:40:02 -0500

    Add questions and variables for vampire survey.

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "Our company serves garlic bread. Should we order some for you (y/n)"
+order = gets.chomp
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+insurance = gets.chomp
+
+puts case
+	when name == ("Drake Cula" || "Tu Fang")
+		puts "Definitely a vampire."
+end 
