2406988e95f486dbc35eaa2ab7a8964d64fcf925
Brian Lin <lin.brian.l@gmail.com>
2017-07-23 23:05:22 -0500

    Start 5.4

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,64 @@
+#Multi-game Fighting Tournament
+tournament = {
+	sfv: {
+		attendees: {
+			names: ["jwong", "infiltration", "daigo"],
+			count: 3
+		}
+		setups: {
+			tables: 10,
+			chairs: 40,
+			outlets: 40,
+			tvs: 20,
+			consoles: 20
+		}
+		streams: ["spooky", "8wayrun"]
+		waves: {
+			a: 30,
+			b: 30
+		}
+	}
+
+	melee: {
+		attendees: {
+			names: ["mango", "hbox", "armada", "pp", "leffen"],
+			count: 5
+		}
+		setups: {
+			tables: 15,
+			chairs: 60,
+			outlets: 70,
+			tvs: 80,
+			consoles: 30
+		}
+		streams: ["vgbc", "meleeeveryday"]
+		waves: {
+			a: 40,
+			b: 50,
+			c: 30
+		}
+	}
+
+	pm: {
+		attendees: {
+			names: ["switch", "sothe", "hyperflame", "techboy", "llod", "lunchables"],
+			count: 6
+		}
+		setups: {
+			tables: 13,
+			chairs: 55,
+			outlets: 60,
+			tvs: 50,
+			consoles: 25
+		}
+		streams: ["azpm", "balcony", "shinecorp"]
+		waves: {
+			a: 40,
+			b: 50,
+			c: 30,
+			d: 45
+		}
+	}
+}
+
+p tournament
