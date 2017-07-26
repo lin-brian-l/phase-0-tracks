acb8c16ee7e29343b1d3f84fe2295dbaf1687ff5
Brian Lin <lin.brian.l@gmail.com>
2017-07-25 21:42:34 -0500

    Started 5.3.

---
+++ ruby/iteration.rb
@@ -0,0 +1,20 @@
+#Release 0 Practice
+
+# def some_method
+#   puts "This is before the block."
+#   array = [1, 2, 3, 4, 5]
+#   yield(array)
+# end
+
+# some_method { |array| puts "Number #{array} is in the block." }
+
+array = [1, 2, 3, 4, 5]
+hash = {
+	"Brian" => "Lin",
+	"Devin" => "Miller",
+	"David" => "Qiu",
+	"Donald" => "Yang"
+}
+
+p array
+p hash
