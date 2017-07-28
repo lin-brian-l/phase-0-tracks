340041eb9f55fb2413dd2098ca1de2207394a5d5
Brian Lin <lin.brian.l@gmail.com>
2017-07-27 23:39:17 -0500

    Update vampires.rb
    @Bontgoy I've made changes to the code based on your 1st comment (using the Times class for the current year) and 3rd comment (putting the allergy logic ahead of the results processing), but I'm still not sure how to address your 2nd comment (refactoring the multiple if statements into one if/elsif/else loop). The original instructions say to fulfill the conditions "in the order they're listed", but the 2nd and 3rd condition are what give me trouble. I would've done an if/elsif/else loop, but since the 3rd condition (which involves getting the age wrong, hating garlic bread, and not wanting insurance) also matches the second condition (getting the age wrong, and hates garlic bread or waives insurance), the 2nd condition will always override the 3rd condition if they're put in the same if/elsif loop. Is there something I'm missing, or should I ignore the original instructions and use an if/elsif loop and re-order the conditions as needed?

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -57,4 +57,4 @@ repeat.times do
 	puts
 end
 
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
