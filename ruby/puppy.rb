a08ef948b52c077bd41fe34a86d4ca3e5b2d77be
Brian Lin <lin.brian.l@gmail.com>
2017-07-29 02:17:11 -0500

    Finish 6.1

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -16,3 +16,16 @@
 
 class Puppy
 end
+
+# Release 1
+p Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+p spot.class
+p duchess == fido
+p fido.instance_of?(Array)
+p fido.instance_of?(Puppy)
+spot.play_dead
