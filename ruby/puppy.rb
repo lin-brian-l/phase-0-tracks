09d04e275412ec38e5da0f0b6d59ea61d8fec1ba
Brian Lin <lin.brian.l@gmail.com>
2017-07-29 02:11:10 -0500

    Finish release 0, start release 1.

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Release 0
+# Species ------------------
+# American Eskimo
+
+# Characteristics ----------
+# Fur color: white
+# Female Size: small
+# Male Size: Large
+# Name: Varies
+
+# Behavior ----------------
+# Lick
+# Eat
+# Bark
+# Jump
+
+class Puppy
+end
