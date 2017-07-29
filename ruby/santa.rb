d3884b0e086a2e5b10ec1e5353e1d2089c34f733
Brian Lin <lin.brian.l@gmail.com>
2017-07-29 03:02:13 -0500

    Complete release 1.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,11 @@
 class Santa
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -6,14 +13,23 @@ class Santa
 	def eat_milk_and_cookies(x)
 		puts "That was a good #{x}!"
 	end
-
-	def initializ #"initialize" was already a private method, so name couldn't be used.
-		puts "Initializing Santa instance..."
-	end
+	
 end
 
-santa1 = Santa.new
+# Release 0
+# santa1 = Santa.new
 
-santa1.speak
-santa1.eat_milk_and_cookies("chocolate chip")
-santa1.initializ
+# santa1.speak
+# santa1.eat_milk_and_cookies("chocolate chip")
+# santa1.initialize
+
+# Release 1
+santas = {
+	"male" => "Asian",
+	"female" => "white",
+	"bigender" => "middle-eastern",
+	"n/a" => "Hispanic"
+}
+
+record = []
+santas.each { |gender, ethnicity| record << Santa.new(gender, ethnicity) }
