7cd1abf708cbd9ae8f99497d0966ad165bf41209
Brian Lin <lin.brian.l@gmail.com>
2017-08-02 15:07:21 -0500

    Finish 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -25,9 +25,42 @@ class Puppy
     end
   
 end
-puppy = Puppy.new("Sparky")
+# puppy = Puppy.new
 #puppy.fetch("Toy")
 #puppy.speak(5)
 #puppy.roll_over
 #puppy.dog_years(3)
 #puppy.stay
+
+class Cats
+	def initialize(name)
+		@name = name 
+		puts "Meow!"
+	end
+
+	def catch(number)
+		puts "#{@name} caught #{number} mice!"
+	end
+
+	def yarn
+		puts "*#{@name} plays with yarn*"
+	end
+end
+
+data_base = []
+kitties = ["cat", 1]
+placeholder = kitties.join
+
+50.times do
+	cat = Cats.new(placeholder)
+	data_base << cat
+	kitties[1] = kitties[1].next
+	placeholder = kitties.join
+end
+
+data_base.each do | i |
+	i.catch(2)
+	i.yarn
+end
+
+p data_base
