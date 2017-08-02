5da5114e52fe41c43b48307f1c5b5af9435ed697
Brian Lin <lin.brian.l@gmail.com>
2017-08-02 14:32:02 -0500

    Start 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,33 @@
 class Puppy
-
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
+    def initialize
+        @name = name
+        puts "Initializing new puppy instance ..."
+    end
+    def fetch(toy)
+        puts "#{@name} brought back the #{toy}!"
+        puts "I brought back the #{toy}!"
+        toy
+    end
+    def speak(number)
+    p "Woof! "*number
+    end
+  
+    def roll_over
+     puts "*rolls over*"
+    end
+  
+    def dog_years(number)
+     return number*7
+    end
+  
+    def stay
+        puts "*stay*"
+    end
+  
 end
-
+puppy = Puppy.new("Sparky")
+#puppy.fetch("Toy")
+#puppy.speak(5)
+#puppy.roll_over
+#puppy.dog_years(3)
+#puppy.stay
