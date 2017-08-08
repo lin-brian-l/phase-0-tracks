b9c228823216b4b4a3677ee1759dc2310973e9ba
Brian Lin <lin.brian.l@gmail.com>
2017-08-08 17:54:55 -0500

    Finish 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,4 @@
+// ***************Release 1**************
 var colors = ["red", "white", "blue", "green"];
 var horse_names = ["Ed", "Francis", "Zach", "Brian"];
 colors.push("iridescent blue/green with silver sparkles");
@@ -11,3 +12,41 @@ for (var i = 0; i < colors.length; i++ ) {
 }
 
 console.log(horse_info);
+console.log(Object.keys(horse_info));
+console.log(horse_info[Object.keys(horse_info)]);
+
+// ***************Release 2**************
+// function Car(make, model, age, mileage) {
+// 	this.make = make;
+// 	this.model = model;
+// 	this.age = age;
+// 	this.mileage = mileage;
+
+// 	this.rating = function() {
+// 		var rating = "";
+// 		if (this.mileage < 10000) { 
+// 			rating = "excellent"; 
+// 		} else if (this.mileage < 30000) {
+// 			rating = "good";
+// 		} else if (this.mileage < 80000) {
+// 			rating = "fair";
+// 		} else {
+// 			rating = "poor";
+// 		}
+// 		return rating
+// 	}
+// }
+
+// var car1 = new Car("Honda", "Civic", "10", 114000)
+// console.log(car1)
+// console.log(car1.rating())
+
+// var car2 = new Car("Toyota", "Camry", "5", 70000)
+// console.log(car2)
+// console.log(car2.make)
+// console.log(car2.model)
+
+// var car3 = new Car("Slave1", "Boba Fett", "30", 2500000)
+// console.log(car3)
+// console.log(car3.age)
+// console.log(car3.mileage)
